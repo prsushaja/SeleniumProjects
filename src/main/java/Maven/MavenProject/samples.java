2aae8c4cd2eed43fa273b1f3dd6f75c1a6840e6f
SUSHAJA PR <prsushaja@gmail.com>
2023-05-03 21:59:36 +0530

    new sample file added

---
+++ src/main/java/Maven/MavenProject/samples.java
@@ -0,0 +1,10 @@
+package Maven.MavenProject;
+
+public class samples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Maven project");
+	}
+
+}
