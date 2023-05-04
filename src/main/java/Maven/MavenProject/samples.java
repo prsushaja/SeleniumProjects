0472ebf3332de7a8de7eb231cb10c151a11d8779
SUSHAJA PR <prsushaja@gmail.com>
2023-05-04 07:54:24 +0530

    new changes

--- src/main/java/Maven/MavenProject/samples.java
+++ src/main/java/Maven/MavenProject/samples.java
@@ -5,6 +5,7 @@ public class samples {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Maven project");
+		System.out.println("project");
 	}
 
 }
